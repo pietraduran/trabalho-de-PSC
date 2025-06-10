3622f56a06565df2463aa8ff8a3eaaeea707871d
Pietra Duran Fonseca Nogueira Britto <pietraduranbritto@gmail.com>
2025-06-10 10:58:59 -0300

    Update Cliente.java

--- Trabalho/src/Cliente.java
+++ Trabalho/src/Cliente.java
@@ -1,6 +1,33 @@
-public class Cliente extends Pessoa {
+public class Cliente {
+    private String nome;
+    private String cpf;
+    
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
+  
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    
+  
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
 
-    public Cliente (String nome, String cpf){
-        super(nome, cpf);
+    @Override
+    public String toString() {
+        return "Cliente{nome='" + nome + "', cpf='" + cpf + "'}";
     }
 }
